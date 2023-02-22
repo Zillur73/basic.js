0b49de828884a5ce9baff06ce9d27b2ff289cb41
Mohammad Zillur Rahaman <mdzillur.cse@gmail.com>
2023-02-22 12:13:58 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,35 @@
+// number declair
+let number=20;
+console.log(number);
+// string declair
+let myName="Zillur";
+console.log(myName);
+//boolean
+
+let value=false;
+console.log(value);
+
+// Value assign
+
+let number1=73;
+let number2=53;
+
+// addition
+
+let sumTotal=number1+number2;
+console.log(sumTotal);
+
+//Subtraction
+
+let sub=number1-number2;
+console.log(sub);
+// division
+
+let divResult=number1/number2;
+console.log(divResult);
+
+//multiplication
+
+let multiTotal=number1*number2;
+console.log(multiTotal);
+
